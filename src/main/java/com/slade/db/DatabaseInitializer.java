b604f7bf96d14a0407f55d12d2230536a2970b57
Qingdao88 <Qingdao88@proton.me>
2025-02-15 15:59:43 +0800

    读取配置文件；创建数据库

---
+++ src/main/java/com/slade/db/DatabaseInitializer.java
@@ -0,0 +1,48 @@
+package com.slade.db;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+
+public class DatabaseInitializer {
+
+    public static final String JDBC_PROPERTIES_PATH = "jdbc.properties";
+
+    public static void createDatabase() {
+        Properties properties = getProperties();
+        String SERVER_URL = properties.getProperty("SERVER_URL");
+        String USERNAME = properties.getProperty("USERNAME");
+        String PASSWORD = properties.getProperty("PASSWORD");
+        String createDatabaseSql = """
+                    CREATE DATABASE IF NOT EXISTS debt_manager
+                    CHARACTER SET utf8mb4
+                    COLLATE utf8mb4_unicode_ci;
+                """;
+        try (Connection connection = DriverManager.getConnection(SERVER_URL, USERNAME, PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(createDatabaseSql)) {
+            preparedStatement.executeUpdate();
+            System.out.println("数据库创建成功或已存在。");
+        } catch (Exception e) {
+            throw new RuntimeException("创建数据库出错", e);
+        }
+    }
+
+    private static Properties getProperties() {
+        // 创建了一个 Properties 对象，用于存储从 jdbc.properties 文件中加载的键值对配置。
+        Properties properties = new Properties();
+        // 将 InputStream 放入 try 语句的括号中，确保使用完毕后自动关闭资源，防止资源泄漏。
+        // 明确告诉 JVM：“请用加载 DatabaseInitializer 这个类的类加载器来查找资源”，这样可以确保读取到债务管理模块自己的 jdbc.properties 文件，从而保证正确的数据库配置信息被加载。
+        try (InputStream inputStream = DatabaseInitializer.class.getClassLoader().getResourceAsStream(JDBC_PROPERTIES_PATH)) {
+            if (inputStream == null) {
+                throw new RuntimeException("找不到配置文件：" + JDBC_PROPERTIES_PATH);
+            }
+            properties.load(inputStream);
+        } catch (Exception e) {
+            throw new RuntimeException("加载配置文件出错：" + JDBC_PROPERTIES_PATH, e);
+        }
+        return properties;
+    }
+
+}
---
+++ src/test/java/com/slade/db/DatabaseInitializerTest.java
@@ -0,0 +1,12 @@
+package com.slade.db;
+
+import org.junit.jupiter.api.Test;
+
+public class DatabaseInitializerTest {
+
+    @Test
+    void testCreateDatabase() {
+        DatabaseInitializer.createDatabase();
+    }
+
+}
